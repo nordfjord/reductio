2ec9426d7a7f5f4a342a66b748f56731f75d0ca4
Einar Norðfjörð <einar@vortex.is>
2015-10-03 23:54:43 +0000

    better demo

--- demo/demo.js
+++ demo/demo.js
@@ -24,31 +24,36 @@ makeDimGroup(crossfilter(demo_data[0]), d3.extent(demo_data[0], function(d){retu
 
 chart
     .height(200)
-    .width(800)
     .brushOn(false)
     .valueAccessor(function(d){return d.value.sum;})
     .elasticY(true)
     .transitionDuration(0);
 
+chart.margins().right = 5;
 chart.render();
 
 var threshold_input = document.getElementById('threshold');
+var threshold_value = document.getElementById('threshold_value');
+var total_count = document.getElementById('total_count');
 var demo_selector = document.getElementById('demo_id');
 
 demo_selector.addEventListener('change', function(ev){
     var demo = ev.target.value;
-    console.log(demo);
-    makeDimGroup(crossfilter(demo_data[demo]), d3.extent(demo_data[demo], function(d){
+    var extent = d3.extent(demo_data[demo], function(d){
         return d[0];
-    }));
+    });
+    makeDimGroup(crossfilter(demo_data[demo]), extent);
     chart.group(group).dimension(dimension);
     threshold_input.value = '1000';
+    threshold_value.innerHTML = '1000';
+    total_count.innerHTML = extent[1];
     chart.redraw();
 });
 
 threshold_input.addEventListener('input', function(ev){
     var threshold = +ev.target.value;
+    threshold_value.innerHTML = threshold;
     reducer.threshold(threshold);
-    console.log(threshold);
+
     chart.redraw();
 });
